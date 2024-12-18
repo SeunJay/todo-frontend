54793f7a5b1f2da94e1ed7d0baaac31513c9bde9
Oluwaseun Jayeoba <seunjay92@gmail.com>
2024-12-18 07:14:17 +0100

    implement color selector component

---
+++ components/ColorSelector.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useState } from "react";
+
+interface Props {
+  label: string;
+  color: string;
+  colors: string[];
+  handleSelectColor: (color: string) => void;
+}
+
+const ColorSelector = ({ label, colors, color, handleSelectColor }: Props) => {
+  return (
+    <div className="w-full flex flex-col gap-[12px] h-[81px]">
+      <label className="text-[#4EA8DE] text-[14px] font-bold">{label}</label>
+
+      <div className="flex items-center gap-[15px] w-full">
+        {colors.map((bgColor, i) => (
+          <span
+            className={
+              color === bgColor
+                ? `h-[30px] md:h-[52px] w-[30px] md:w-[52px] rounded-full !border-2 !border-[#fff] cursor-pointer`
+                : `h-[30px] md:h-[52px] w-[30px] md:w-[52px] rounded-full cursor-pointer`
+            }
+            style={{ backgroundColor: bgColor }}
+            key={i}
+            onClick={() => handleSelectColor(bgColor)}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ColorSelector;
